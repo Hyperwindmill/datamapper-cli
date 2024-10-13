f2d6507931a651822fc3d28801de7112fe7c536b
Daniele Traverso <sweat2004@gmail.com>
2024-10-14 00:05:27 +0200

    word processing

--- src/core/Query.ts
+++ src/core/Query.ts
@@ -5,15 +5,29 @@ interface Source {
 export default class Query {
   query: string = ''
   staticSource?: Source
+  words: string[] = []
   constructor(query: string, staticSource?: Source) {
     this.query = query
     this.staticSource = staticSource
-    this.analize();
+    this.analize()
   }
-  private analize(){
-    const words= this.query.split(/\W+/);
+  private nextWord() {
+    let word = this.words.shift()
+    if (word) {
+      if ((word.startsWith('"') && word.endsWith('"')) || (word.startsWith("'") && word.endsWith("'"))) {
+        word = word.slice(1, -1)
+      }
+    }
+    return word
   }
-  public run(){
-    
+  private analize() {
+    console.log(this.query)
+    // Match words or quoted phrases
+    this.words = this.query.match(/"[^"]*"|'[^']*'|\w+/g) || []
+    let word: string | undefined
+    while ((word = this.nextWord())) {
+      console.log(word)
+    }
   }
+  public run() {}
 }
