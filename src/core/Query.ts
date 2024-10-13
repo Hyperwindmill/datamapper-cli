c95dcf514896b7090ec42da3fd71f506823996a4
Daniele Traverso <sweat2004@gmail.com>
2024-10-13 22:23:24 +0200

    Update Query.ts

--- src/core/Query.ts
+++ src/core/Query.ts
@@ -8,6 +8,10 @@ export default class Query {
   constructor(query: string, staticSource?: Source) {
     this.query = query
     this.staticSource = staticSource
+    this.analize();
+  }
+  private analize(){
+    const words= this.query.split(/\W+/);
   }
   public run(){
     
